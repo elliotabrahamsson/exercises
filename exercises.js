b68b2c7cda45b8714ada31fdc22e906b7c2368af
elliotabrahamsson <elliotsson@outlook.com>
2022-09-15 10:18:46 +0200

    structure in place

---
+++ exercises.js
@@ -0,0 +1,8 @@
+console.log('I am alive');
+
+function exercise1(){
+    console.log('first exercise...');
+    var textbox1value = document.getElementById('txt1').value;
+    var textbox2value = document.getElementById('txt2').value;
+    console.log(`${textbox1value}, ${textbox2value} (${textbox1value.length})`);
+}
