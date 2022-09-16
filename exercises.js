576f22d070a9b516c48aa34191b90658b3b86159
elliotabrahamsson <elliotsson@outlook.com>
2022-09-16 09:07:06 +0200

    intial commit

--- exercises.js
+++ exercises.js
@@ -1,8 +1,23 @@
 console.log('I am alive');
 
 function exercise1(){
-    console.log('first exercise...');
     var textbox1value = document.getElementById('txt1').value;
-    var textbox2value = document.getElementById('txt2').value;
-    console.log(`${textbox1value}, ${textbox2value} (${textbox1value.length})`);
+    const words = textbox1value.split(' ');
+    console.log(words.length);
+
+    words.forEach(word => {
+        console.log(`${word} (${word.length})`);
+    });
+
+}
+
+function exercise2() {
+    var textbox1value = document.getElementById('txt1').value;
+    const words =textbox1value.split(' ');
+
+    console.log(words.length);
+
+    words.forEach(word => {
+        console.log(`${word} (${words.reverse})`);
+    });
 }
